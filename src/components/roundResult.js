f6cb87ff4838000f243811b5db79aac2be3ecbd1
Daria Tabala <dasha.tabala@gmail.com>
2019-04-01 19:22:59 +0300

    fix scale of buttons

--- src/components/roundResult.js
+++ src/components/roundResult.js
@@ -3,7 +3,7 @@ import { div, text, button, h1 } from '../lib/dom'
 const roundResult = (result, onNext) => {
     return div({ className: `round-result` },
         h1({}, text(result)),
-        button({ onclick: onNext, className: `next-button` }, text(`Next question`)))
+        button({ onclick: onNext, className: `next-button` }, text(`Next`)))
 }
 
 export default roundResult
